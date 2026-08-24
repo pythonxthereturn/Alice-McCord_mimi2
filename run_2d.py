b119a7584c9cb30c175c1447bf2fa8408771426a
python × The Return <python_x_thereturn@qq.com>
2026-08-25 04:25:03 +0800

    Delete run_2d.py

--- run_2d.py
+++
@@ -1,7 +0,0 @@
-import sys
-from main import load_or_create_system, interactive_loop
-
-if __name__ == "__main__":
-    container_name = sys.argv[1] if len(sys.argv) > 1 else "default"
-    system = load_or_create_system(container_name)
-    interactive_loop(system, container_name)
